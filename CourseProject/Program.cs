3360b7a15ac2827ff86763ee6aec2fc784887e6b
kille <kille@DESKTOP-EJAPLE9>
2023-06-15 18:56:34 +0300

    css fix

--- CourseProject/Program.cs
+++ CourseProject/Program.cs
@@ -29,13 +29,21 @@ app.UseRouting();
 
 app.UseAuthorization();
 
-app.MapControllerRoute(
-    name: "MyArea",
+app.UseEndpoints(endpoints =>
+{/*
+    endpoints.MapControllerRoute(
+      name: "default",
+      pattern: "{area:exists}/{controller=Course}/{action=SaveNew}/{id?}");
+    */
+
+    endpoints.MapControllerRoute(
+    name: "default",
     pattern: "{area:exists}/{controller=Home}/{action=Index}/{id?}");
 
-app.MapControllerRoute(
-    name: "default",
-    pattern: "{controller=Home}/{action=Index}/{id?}");
+    endpoints.MapControllerRoute(
+        name: "default",
+        pattern: "{controller=Home}/{action=Index}/{id?}");
+});
 
 
 app.Run();
